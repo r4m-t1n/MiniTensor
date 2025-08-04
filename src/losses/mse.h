948ca363d7d55d2f978250d6ed4f33edbaff53e0
r4m-t1n <im.r4mt1n@gmail.com>
2025-08-04 23:39:18 +0330

    rewrite with cpp

--- src/losses/mse.h
+++ src/losses/mse.h
@@ -1,8 +1,18 @@
 #ifndef MSE_H
 #define MSE_H
 
-int mse_loss_int(int y, int y_hat);
-float mse_loss_float(float y, float y_hat);
-double mse_loss_double(double y, double y_hat);
+#include <iostream>
+#include "tensors/tensor.h"
+
+template<typename T>
+T mse_loss(const Tensor<T>& y, const Tensor<T>& y_hat) {
+    check_tensor_validity(y, y_hat);
+    T loss = static_cast<T>(0);
+    for (int i = 0; i < y.size; ++i) {
+        T diff = y.data[i] - y_hat.data[i];
+        loss += diff * diff;
+    }
+    return loss / static_cast<T>(y.size);
+}
 
 #endif
