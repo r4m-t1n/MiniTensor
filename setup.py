20d69b33d97b5200a7fe24fdcc82937af9ab08f5
r4m-t1n <im.r4mt1n@gmail.com>
2025-08-11 19:46:10 +0330

    fix setup extension

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup, Extension
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 import pybind11
 
 ext_modules = [
-    Extension(
-        'minitensor',
-        ['python_binding/bindings.cpp'],
-        include_dirs=[pybind11.get_include()],
-        language='c++',
+    Pybind11Extension(
+        "minitensor.minitensor_cpp",
+        ["python_binding/bindings.cpp"],
+        include_dirs=[
+            pybind11.get_include(),
+            "core"
+        ],
+        cxx_std=17,
     ),
 ]
 
 setup(
-    name='minitensor',
-    version='0.1',
-    author='r4m-t1n',
-    packages=['python'],
+    name="minitensor",
+    version="0.1",
+    author="r4m-t1n",
+    packages=["minitensor"],
     ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
 )
