18d940a1cacf4a67350b0cffd78702d088ab09e5
r4m-t1n <im.r4mt1n@gmail.com>
2025-08-18 23:58:38 +0330

    add requires_grad

--- minitensor/tensor.py
+++ minitensor/tensor.py
@@ -7,29 +7,29 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 from . import minitensor_cpp as mtc
 
 class Tensor:
-    def __init__(self, data: List, shape: List[int], dtype: str):
+    def __init__(self, data: List, shape: List[int], dtype: str, requires_grad: bool = False):
         if not data:
             raise ValueError("ERROR: Data cannot be empty.")
         elif not shape:
             raise ValueError("ERROR: Shape cannot be empty.")
 
         first_el = data[0]
-        if (not isinstance(first_el, int)) and (not isinstance(first_el, float)):
+        if not isinstance(first_el, (int, float)):
             raise TypeError("ERROR: Unsupported data type. Only int, float and double are supported.")
 
         self.dtype = dtype
         if (dtype == "int32") or (dtype == "int"):
-            self._tensor = mtc.int32.Tensor(data, shape)
+            self._tensor = mtc.int32.Tensor(data, shape, requires_grad)
 
         elif (dtype == "float32") or (dtype == "float"):
-            self._tensor = mtc.float32.Tensor(data, shape)
+            self._tensor = mtc.float32.Tensor(data, shape, requires_grad)
 
         elif (dtype == "float64") or (dtype == "double"):
-            self._tensor = mtc.float64.Tensor(data, shape)
+            self._tensor = mtc.float64.Tensor(data, shape, requires_grad)
 
         else:
             raise TypeError("ERROR: Unsupported data type. Only int, float and double are supported.")
-
+    
     @property
     def shape(self) -> tuple:
         return tuple(self._tensor.shape)
@@ -46,68 +46,91 @@ class Tensor:
     def stride(self) -> tuple:
         return self._tensor.stride
 
+    @property
+    def requires_grad(self) -> bool:
+        return self._tensor.requires_grad
+
+    @requires_grad.setter
+    def requires_grad(self, value: bool):
+        self._tensor.requires_grad = value
+
+    @property
+    def grad(self):
+        if self.requires_grad and self._tensor.grad:
+            new_tensor = Tensor.__new__(Tensor)
+            new_tensor._tensor = self._tensor.grad
+            new_tensor.dtype = self.dtype
+            return new_tensor
+        return None
+
+    def backward(self):
+        self._tensor.backward()
+
     def to_list(self) -> list:
         return self._tensor.to_list()
 
-    def _new_tensor(self, result):
+    def _new_tensor(self, result, requires_grad: bool):
         new_python_tensor = Tensor.__new__(Tensor)
         new_python_tensor._tensor = result
         new_python_tensor.dtype = self.dtype
+        if requires_grad:
+            new_python_tensor.requires_grad = True
         return new_python_tensor
 
     def __repr__(self):
         return repr(self._tensor)
-    
+
     def __matmul__(self, other):
         if isinstance(other, Tensor):
             result = self._tensor @ other._tensor
+            requires_grad = self.requires_grad or other.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for @: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __add__(self, other):
         if isinstance(other, Tensor):
             result = self._tensor + other._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = self._tensor + other
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for +: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __sub__(self, other):
         if isinstance(other, Tensor):
             result = self._tensor - other._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = self._tensor - other
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for -: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __mul__(self, other):
         if isinstance(other, Tensor):
             result = self._tensor * other._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = self._tensor * other
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for *: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __truediv__(self, other):
         if isinstance(other, Tensor):
             result = self._tensor / other._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = self._tensor / other
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for /: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -115,13 +138,13 @@ class Tensor:
     def __rsub__(self, other):
         if isinstance(other, Tensor):
             result = other._tensor - self._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = other - self._tensor
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for -: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -129,15 +152,15 @@ class Tensor:
     def __rtruediv__(self, other):
         if isinstance(other, Tensor):
             result = other._tensor / self._tensor
-
+            requires_grad = self.requires_grad or other.requires_grad
         elif isinstance(other, (int, float)):
             result = other / self._tensor
-
+            requires_grad = self.requires_grad
         else:
             raise TypeError(f"ERROR: Unsupported operand type for /: 'Tensor' and '{type(other).__name__}'")
-        return self._new_tensor(result)
+        return self._new_tensor(result, requires_grad)
 
-def tensor(data: list, shape: list = None, dtype: str = None) -> Tensor:
+def tensor(data: list, shape: list = None, dtype: str = None, requires_grad: bool = False) -> Tensor:
     if not data:
         raise ValueError("ERROR: Data cannot be empty.")
 
@@ -154,13 +177,13 @@ def tensor(data: list, shape: list = None, dtype: str = None) -> Tensor:
         shape = [len(data)]
 
     if (dtype == "int32") or (dtype == "int"):
-        return Tensor(data, shape, "int")
+        return Tensor(data, shape, "int", requires_grad)
 
     elif (dtype == "float32") or (dtype == "float"):
-        return Tensor(data, shape, "float")
+        return Tensor(data, shape, "float", requires_grad)
 
     elif (dtype == "float64") or (dtype == "double"):
-        return Tensor(data, shape, "double")
+        return Tensor(data, shape, "double", requires_grad)
 
     else:
         raise TypeError("ERROR: Unsupported data type. Only int, float and double are supported.")
