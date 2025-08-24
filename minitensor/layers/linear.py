56d2a3947dcef72d3d40cf4a49e1eff8d1eba3e4
r4m-t1n <im.r4mt1n@gmail.com>
2025-08-24 23:48:03 +0330

    add Linear class to python

--- minitensor/layers/linear.py
+++ minitensor/layers/linear.py
@@ -1,7 +1,31 @@
 import os
 import sys
+from typing import Generator
 
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from . import minitensor_cpp as mtc
+from minitensor import Tensor
+
 class Linear:
-    pass
+    def __init__(self,
+        input_features: int, output_features: int,
+        activation: str = None):
+        self.input_f = input_features
+        self.output_f = output_features
+
+        self._linear = mtc.Linear(
+            self.input_f, self.output_f, activation)
+    
+    @property
+    def weight(self) -> Tensor:
+        return self._linear.weight
+
+    @property
+    def bias(self) -> Tensor:
+        return self._linear.bias
+
+    def parameters(self) -> Generator[Tensor]:
+        for param in self._linear.parameters():
+            yield param
