a0c04914ba3d32da721a2150124f8663aaec74b4
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-10 23:47:55 +0330

    fix bugs

--- core/tensors/tensor_broadcast.h
+++ core/tensors/tensor_broadcast.h
@@ -1,8 +1,11 @@
+#ifndef TENSOR_BROADCAST_H
+#define TENSOR_BROADCAST_H
+
 #include <vector>
 #include <stdexcept>
 #include <algorithm>
 #include "tensor.h"
- 
+
 inline std::vector<int> broadcast_shape(const std::vector<int>& shape_a, const std::vector<int>& shape_b) {
     int ndim_a = shape_a.size();
     int ndim_b = shape_b.size();
@@ -23,26 +26,25 @@ inline std::vector<int> broadcast_shape(const std::vector<int>& shape_a, const s
 
 template<typename T>
 Tensor<T> expand_tensor(const Tensor<T>& tensor, const std::vector<int>& target_shape) {
-    if (tensor.shape == target_shape) {
-        return tensor;
-    }
-
     Tensor<T> result(target_shape, tensor.requires_grad);
     int target_ndim = target_shape.size();
     int tensor_ndim = tensor.ndim;
 
     std::vector<int> broadcast_strides(target_ndim, 0);
-    for (int i = 1; i <= tensor_ndim; ++i) {
-        if (tensor.shape[tensor_ndim - i] == target_shape[target_ndim - i]) {
-            broadcast_strides[target_ndim - i] = tensor.stride[tensor_ndim - i];
+    int shape_diff = target_ndim - tensor_ndim;
+    for (int i = 0; i < tensor_ndim; ++i) {
+        if (tensor.shape[i] == target_shape[i + shape_diff]) {
+            broadcast_strides[i + shape_diff] = tensor.stride[i];
         }
     }
 
     for (int i = 0; i < result.size; ++i) {
         int original_index = 0;
         int temp_i = i;
-        for (int j = 0; j < target_ndim; ++j) {
-            int coord = (temp_i / result.stride[j]) % target_shape[j];
+        for (int j = target_ndim - 1; j >= 0; --j) {
+            int current_dim_size = result.shape[j];
+            int coord = temp_i % current_dim_size;
+            temp_i /= current_dim_size;
             original_index += coord * broadcast_strides[j];
         }
         result.data[i] = tensor.data[original_index];
@@ -56,5 +58,7 @@ std::pair<Tensor<T>, Tensor<T>> broadcast(const Tensor<T>& a, const Tensor<T>& b
     std::vector<int> result_shape = broadcast_shape(a.shape, b.shape);
     Tensor<T> a_broadcasted = expand_tensor(a, result_shape);
     Tensor<T> b_broadcasted = expand_tensor(b, result_shape);
-    return {a_broadcasted, b_broadcasted};
+    return { std::move(a_broadcasted), std::move(b_broadcasted) };
 }
+
+#endif
