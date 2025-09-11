7aa0f4a69b95b95ddccd21fc0ae9063ee8eb699a
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-11 23:11:32 +0330

    migrate from raw pointers to std::shared_ptr

--- core/nn/activations/relu.h
+++ core/nn/activations/relu.h
@@ -1,18 +1,22 @@
 #ifndef RELU_H
 #define RELU_H
 
+#include <memory>
+#include <vector>
 #include "tensors/tensor.h"
 #include "autograd/autograd_activations.h"
 
 template<typename T>
-Tensor<T> relu(Tensor<T>& tensor) {
-    Tensor<T> result(tensor.shape, tensor.requires_grad);
-    for (int i = 0; i < result.size; ++i) {
-        result.data[i] = (tensor.data[i] > 0) ? tensor.data[i] : static_cast<T>(0);
+std::shared_ptr<Tensor<T>> relu(std::shared_ptr<Tensor<T>> tensor) {
+    auto result = std::make_shared<Tensor<T>>(tensor->shape, tensor->requires_grad);
+
+    for (int i = 0; i < result->size; ++i) {
+        result->data[i] = (tensor->data[i] > 0) ? tensor->data[i] : static_cast<T>(0);
     }
-    if (tensor.requires_grad) {
-        result.parents.push_back(&tensor);
-        result.grad_fn = std::make_unique<ReluBackward<T>>(&tensor);
+
+    if (tensor->requires_grad) {
+        result->parents.push_back(tensor);
+        result->grad_fn = std::make_unique<ReluBackward<T>>(tensor);
     }
     return result;
 }
--- core/nn/activations/tanh.h
+++ core/nn/activations/tanh.h
@@ -1,19 +1,23 @@
 #ifndef TANH_H
 #define TANH_H
 
+#include <cmath>
+#include <memory>
+#include <vector>
 #include "tensors/tensor.h"
 #include "autograd/autograd_activations.h"
-#include <cmath>
 
 template<typename T>
-Tensor<T> tanh(Tensor<T>& tensor) {
-    Tensor<T> result(tensor.shape, tensor.requires_grad);
-    for (int i = 0; i < result.size; ++i) {
-        result.data[i] = std::tanh(tensor.data[i]);
+std::shared_ptr<Tensor<T>> tanh_fn(std::shared_ptr<Tensor<T>> tensor) {
+    auto result = std::make_shared<Tensor<T>>(tensor->shape, tensor->requires_grad);
+
+    for (int i = 0; i < result->size; ++i) {
+        result->data[i] = std::tanh(tensor->data[i]);
     }
-    if (tensor.requires_grad) {
-        result.parents.push_back(&tensor);
-        result.grad_fn = std::make_unique<TanhBackward<T>>(&tensor, &result);
+
+    if (tensor->requires_grad) {
+        result->parents.push_back(tensor);
+        result->grad_fn = std::make_unique<TanhBackward<T>>(tensor, result);
     }
     return result;
 }
