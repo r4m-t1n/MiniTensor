674ad078d8e183ebe7e60222a836c1f21a8cca1d
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-11 23:11:59 +0330

    migrate from raw pointers to std::shared_ptr

--- core/losses/mae.h
+++ core/losses/mae.h
@@ -2,30 +2,32 @@
 #define MAE_H
 
 #include <cmath>
+#include <memory>
+#include <stdexcept>
 #include "tensors/tensor.h"
 #include "tensors/tensor_ops.h"
 #include "autograd/autograd_losses.h"
 
 template<typename T>
-Tensor<T> mae_loss(Tensor<T>& y, Tensor<T>& y_hat) {
+std::shared_ptr<Tensor<T>> mae_loss(std::shared_ptr<Tensor<T>> y, std::shared_ptr<Tensor<T>> y_hat) {
     check_tensor_validity(y, y_hat);
     T loss_val = static_cast<T>(0);
-    for (int i = 0; i < y_hat.size; ++i) {
-        loss_val += std::abs(y_hat.data[i] - y.data[i]);
+    for (int i = 0; i < y_hat->size; ++i) {
+        loss_val += std::abs(y_hat->data[i] - y->data[i]);
     }
-    loss_val /= static_cast<T>(y_hat.size);
+    loss_val /= static_cast<T>(y_hat->size);
 
-    bool result_requires_grad = y.requires_grad || y_hat.requires_grad;
-    Tensor<T> result({loss_val}, {1}, result_requires_grad);
+    bool result_requires_grad = y->requires_grad || y_hat->requires_grad;
+    
+    auto result = std::make_shared<Tensor<T>>(std::vector<T>{loss_val}, std::vector<int>{1}, result_requires_grad);
 
-    if (result.requires_grad) {
-        result.parents.push_back(&y);
-        result.parents.push_back(&y_hat);
-        result.grad_fn = std::make_unique<MaeLossBackward<T>>(&y, &y_hat);
+    if (result->requires_grad) {
+        result->parents.push_back(y);
+        result->parents.push_back(y_hat);
+        result->grad_fn = std::make_unique<MaeLossBackward<T>>(y, y_hat);
     }
 
     return result;
 }
-
 
 #endif
--- core/losses/mse.h
+++ core/losses/mse.h
@@ -1,27 +1,30 @@
 #ifndef MSE_H
 #define MSE_H
 
+#include <memory>
+#include <stdexcept>
 #include "tensors/tensor.h"
 #include "tensors/tensor_ops.h"
 #include "autograd/autograd_losses.h"
 
 template<typename T>
-Tensor<T> mse_loss(Tensor<T>& y, Tensor<T>& y_hat) {
+std::shared_ptr<Tensor<T>> mse_loss(std::shared_ptr<Tensor<T>> y, std::shared_ptr<Tensor<T>> y_hat) {
     check_tensor_validity(y, y_hat);
     T loss_val = static_cast<T>(0);
-    for (int i = 0; i < y.size; ++i) {
-        T diff = y_hat.data[i] - y.data[i];
+    for (int i = 0; i < y->size; ++i) {
+        T diff = y_hat->data[i] - y->data[i];
         loss_val += diff * diff;
     }
-    loss_val /= static_cast<T>(y.size);
+    loss_val /= static_cast<T>(y->size);
 
-    bool result_requires_grad = y.requires_grad || y_hat.requires_grad;
-    Tensor<T> result({loss_val}, {1}, result_requires_grad);
+    bool result_requires_grad = y->requires_grad || y_hat->requires_grad;
+    
+    auto result = std::make_shared<Tensor<T>>(std::vector<T>{loss_val}, std::vector<int>{1}, result_requires_grad);
 
-    if (result.requires_grad) {
-        result.parents.push_back(&y);
-        result.parents.push_back(&y_hat);
-        result.grad_fn = std::make_unique<MseLossBackward<T>>(&y, &y_hat);
+    if (result->requires_grad) {
+        result->parents.push_back(y);
+        result->parents.push_back(y_hat);
+        result->grad_fn = std::make_unique<MseLossBackward<T>>(y, y_hat);
     }
 
     return result;
