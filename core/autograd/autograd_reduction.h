57e8c6ae75408b7728dca5100430e301c892cbbf
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-13 00:21:10 +0330

    migrate from raw pointers to std::shared_ptr

--- core/autograd/autograd_reduction.h
+++ core/autograd/autograd_reduction.h
@@ -1,37 +1,42 @@
 #ifndef AUTOGRAD_REDUCTION_H
 #define AUTOGRAD_REDUCTION_H
 
-#include "tensors/tensor.h"
 #include <vector>
+#include <memory>
+#include "tensors/tensor.h"
 
 template<typename T>
 struct SumBackward : public Function<T> {
-    Tensor<T>* parent_input;
+    std::shared_ptr<Tensor<T>> parent_input;
     std::vector<int> original_shape;
     int axis;
 
-    SumBackward(Tensor<T>* input, int reduction_axis) 
+    SumBackward(std::shared_ptr<Tensor<T>> input, int reduction_axis) 
         : parent_input(input), original_shape(input->shape), axis(reduction_axis) {}
 
-    void backward(const Tensor<T>& grad_out) override {
+    void backward(std::shared_ptr<Tensor<T>> grad_out) override {
         if (parent_input->requires_grad) {
-            Tensor<T> grad_a(original_shape, false);
+            auto grad_a = std::make_shared<Tensor<T>>(original_shape, false);
 
-            if (axis == 0) {
+            if (axis == -1) {
+                 for (int i = 0; i < grad_a->size; ++i) {
+                    grad_a->data[i] = grad_out->data[0];
+                }
+            } else if (axis == 0) {
                 for (int i = 0; i < original_shape[0]; ++i) {
-                    for (int j = 0; j < grad_out.size; ++j) {
-                        int grad_a_index = i * grad_out.size + j;
-                        grad_a.data[grad_a_index] = grad_out.data[j];
+                    for (int j = 0; j < grad_out->size; ++j) {
+                        int grad_a_index = i * grad_out->size + j;
+                        grad_a->data[grad_a_index] = grad_out->data[j];
                     }
                 }
-            }/* else {
-               Not implemented for other axes for now 
-            }*/
+            }
             
-            if (parent_input->grad == nullptr) {
-                parent_input->grad = new Tensor<T>(grad_a.data, grad_a.shape);
+            if (!parent_input->grad) {
+                parent_input->grad = grad_a;
             } else {
-                *(parent_input->grad) = *(parent_input->grad) + grad_a;
+                for (int i = 0; i < parent_input->grad->size; ++i) {
+                    parent_input->grad->data[i] += grad_a->data[i];
+                }
             }
 
             if (parent_input->grad_fn) {
