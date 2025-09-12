d9836504cf0f59b975aefe2318578c5ab39d8d58
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-12 23:55:55 +0330

    migrate from raw pointers to std::shared_ptr

--- core/nn/layers/linear.h
+++ core/nn/layers/linear.h
@@ -1,10 +1,13 @@
 #ifndef LINEAR_H
 #define LINEAR_H
 
+#include <memory>
+#include <vector>
 #include <variant>
 #include <type_traits>
 #include <string>
 #include "tensors/tensor.h"
+#include "tensors/tensor_ops.h"
 #include "nn/initializers/initializers.h"
 
 template<typename T>
@@ -16,16 +19,17 @@ std::string linear_repr(const Linear<T>& linear_layer);
 template<typename T>
 using Initializer = typename std::conditional_t<
     std::is_floating_point_v<T>,
-    std::variant<HeNormal<T>, XavierUniform<T>, Constant_Val<T>>,
-    std::variant<Constant_Val<T>>
+    std::variant<std::shared_ptr<HeNormal<T>>, std::shared_ptr<XavierUniform<T>>, std::shared_ptr<Constant_Val<T>>>,
+    std::variant<std::shared_ptr<Constant_Val<T>>>
 >;
 
 template<typename T>
 class Linear {
 private:
-    Tensor<T> weights;
-    Tensor<T> bias;
-    std::unique_ptr<Tensor<T>> input_cache;
+    std::shared_ptr<Tensor<T>> weights;
+    std::shared_ptr<Tensor<T>> bias;
+    std::shared_ptr<Tensor<T>> input_cache; 
+    
     int input_f;
     int output_f;
     friend std::string linear_repr<T>(const Linear<T>&);
@@ -34,45 +38,26 @@ public:
     Linear(int input_features, int output_features,
            Initializer<T> weight_init,
            Initializer<T> bias_init)
-        : weights({output_features, input_features}, true),
-          bias({1, output_features}, true),
-          input_f(input_features),
-          output_f(output_features) {
+        : input_f(input_features), output_f(output_features) {
         
-        std::visit([this](auto&& arg){ arg.initialize(this->weights); }, weight_init);
-        std::visit([this](auto&& arg){ arg.initialize(this->bias); }, bias_init);
-    }
-
-    Tensor<T> forward(const Tensor<T>& input) {
-        this->input_cache = std::make_unique<Tensor<T>>(input.data, input.shape);
-        Tensor<T> output = mat_mul(input, transpose(this->weights));
-        return output + this->bias;
-    }
-
-    Tensor<T> backward(const Tensor<T>& output_gradient) {
-        Tensor<T> grad_bias = sum(output_gradient, 0);
-        Tensor<T> input_transposed = transpose(*(this->input_cache));
-        Tensor<T> grad_weights = transpose(mat_mul(input_transposed, output_gradient));
-        Tensor<T> weights_transposed = transpose(this->weights);
-        Tensor<T> grad_input = mat_mul(output_gradient, weights_transposed);
-
-        if (this->bias.grad == nullptr) {
-            this->bias.grad = new Tensor<T>(grad_bias.data, grad_bias.shape);
-        } else {
-            *(this->bias.grad) = *(this->bias.grad) + grad_bias;
-        }
-
-        if (this->weights.grad == nullptr) {
-            this->weights.grad = new Tensor<T>(grad_weights.data, grad_weights.shape);
-        } else {
-            *(this->weights.grad) = *(this->weights.grad) + grad_weights;
-        }
+        weights = std::make_shared<Tensor<T>>(std::vector<int>{output_features, input_features}, true);
+        bias = std::make_shared<Tensor<T>>(std::vector<int>{1, output_features}, true);
         
-        return grad_input;
+        std::visit([this](auto&& arg){ arg->initialize(*(this->weights)); }, weight_init);
+        std::visit([this](auto&& arg){ arg->initialize(*(this->bias)); }, bias_init);
     }
 
-    std::vector<Tensor<T>*> parameters() {
-        return {&weights, &bias};
+    std::shared_ptr<Tensor<T>> forward(const std::shared_ptr<Tensor<T>>& input) {
+        this->input_cache = input;
+
+        auto transposed_weights = transpose(this->weights); 
+        
+        auto output = mat_mul(input, transposed_weights);
+        return tensor_add(output, this->bias);
+    }
+
+    std::vector<std::shared_ptr<Tensor<T>>> parameters() {
+        return {weights, bias};
     }
 };
 
@@ -93,6 +78,5 @@ std::string linear_repr(const Linear<T>& linear_layer) {
            ", out_features=" + std::to_string(linear_layer.output_f) +
            ", dtype='" + dtype_name + "')";
 }
-
 
 #endif
