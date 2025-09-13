d50795dccf06b51f9cc13486cbdf6f8d3cf6a099
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-13 23:59:17 +0330

    add bce loss

---
+++ core/losses/bce.h
@@ -0,0 +1,34 @@
+#ifndef BCE_H
+#define BCE_H
+
+#include <cmath>
+#include <memory>
+#include <stdexcept>
+#include "tensors/tensor.h"
+#include "tensors/tensor_ops.h"
+#include "autograd/autograd_losses.h"
+
+template<typename T>
+std::shared_ptr<Tensor<T>> bce_loss(std::shared_ptr<Tensor<T>> y, std::shared_ptr<Tensor<T>> y_hat) {
+    check_tensor_validity(y, y_hat);
+    T loss_val = static_cast<T>(0);
+    for (int i = 0; i < y_hat->size; ++i) {
+        loss_val += -(y->data[i] * std::log(y_hat->data[i]))
+            - ((1 - y->data[i]) * std::log(1 - y_hat->data[i]));
+    }
+    loss_val /= static_cast<T>(y_hat->size);
+
+    bool result_requires_grad = y->requires_grad || y_hat->requires_grad;
+    
+    auto result = std::make_shared<Tensor<T>>(std::vector<T>{loss_val}, std::vector<int>{1}, result_requires_grad);
+
+    if (result->requires_grad) {
+        result->parents.push_back(y);
+        result->parents.push_back(y_hat);
+        result->grad_fn = std::make_unique<BceLossBackward<T>>(y, y_hat);
+    }
+
+    return result;
+}
+
+#endif
