b2f8e18697899ab99216b655d61f3fae57028486
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-15 23:51:54 +0330

    add losses, optims, model, math funcs

--- minitensor/__init__.py
+++ minitensor/__init__.py
@@ -1,2 +1,9 @@
-from .tensor import tensor, Tensor
+from .tensor import Tensor, tensor
 from . import layers
+from . import losses
+from . import optims
+from .model import Model
+from .tensor_math import (
+    sqrt, log, exp, pow,
+    sin, cos, tan
+)
