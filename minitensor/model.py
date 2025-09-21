14aaa1a6072dca7cff77afe8ca5aa93a6fd2e65d
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-21 23:54:23 +0330

    add Module and Sequential, rm Model

--- minitensor/model.py
+++ minitensor/model.py
@@ -1,17 +1,28 @@
 from typing import Generator
 from .tensor import Tensor
-from .layers import Linear
 
-class Model:
+class Module:
     def forward(self, *args):
         raise NotImplementedError
 
     def parameters(self) -> Generator[Tensor, None, None]:
-        for attr_name in dir(self):
-            attr = getattr(self, attr_name)
-            if isinstance(attr, (Linear, Model)):
-                if hasattr(attr, 'parameters'):
-                    yield from attr.parameters()
+        yield from ()
 
     def __call__(self, *args) -> Tensor:
         return self.forward(*args)
+
+class Sequential(Module):
+    def __init__(self, *layers):
+        if not all(isinstance(layer, Module) for layer in layers):
+            raise TypeError("ERROR: All inputs to Sequential must be instances of Module (layers or activations).")
+        self.layers = layers
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = x
+        for layer in self.layers:
+            out = layer(out)
+        return out
+        
+    def parameters(self) -> Generator[Tensor, None, None]:
+        for layer in self.layers:
+            yield from layer.parameters()
