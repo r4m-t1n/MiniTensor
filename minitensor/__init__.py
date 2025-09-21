d9734fd4fd0c5fb2da3c771cd84ae496d8a88964
r4m-t1n <im.r4mt1n@gmail.com>
2025-09-21 23:54:44 +0330

    add activations and Module

--- minitensor/__init__.py
+++ minitensor/__init__.py
@@ -1,8 +1,9 @@
 from .tensor import Tensor, tensor
 from . import layers
+from . import activations
 from . import losses
 from . import optims
-from .model import Model
+from .model import Module
 from .tensor_math import (
     sqrt, log, exp, pow,
     sin, cos, tan
