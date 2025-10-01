9146e34ba6c2f20acfc268b1791bc543d2656a25
r4m-t1n <im.r4mt1n@gmail.com>
2025-10-01 23:54:43 +0330

    fix softmax calculations

--- core/nn/activations/softmax.h
+++ core/nn/activations/softmax.h
@@ -9,11 +9,30 @@
 
 template<typename T>
 std::shared_ptr<Tensor<T>> softmax(std::shared_ptr<Tensor<T>> tensor, int axis = -1) {
+    if(axis != -1 && axis != tensor->ndim - 1) {
+        throw std::runtime_error("ERROR: Softmax currently supports only the last axis");
+    }
 
-    auto max_val_tensor = max(tensor, axis);
-    auto exp_tensor = tensor_exp<T, T>(tensor_sub(tensor, max_val_tensor));
-    auto sum_exp_tensor = sum(exp_tensor, axis);
-    auto result = tensor_div(exp_tensor, sum_exp_tensor);
+    auto max_tensor = max(tensor, axis);
+
+    auto shifted = tensor_sub(tensor, max_tensor);
+
+    auto exp_tensor = tensor_exp<T, T>(shifted);
+
+    auto sum_tensor = std::make_shared<Tensor<T>>(tensor->shape, tensor->requires_grad);
+    for(int i = 0; i < tensor->shape[0]; ++i) { // i will replace this with sum() later
+        T row_sum = 0;
+        for(int j = 0; j < tensor->shape[1]; ++j) {
+            int idx = i * tensor->shape[1] + j;
+            row_sum += exp_tensor->data[idx];
+        }
+        for(int j = 0; j < tensor->shape[1]; ++j) {
+            int idx = i * tensor->shape[1] + j;
+            sum_tensor->data[idx] = row_sum;
+        }
+    }
+
+    auto result = tensor_div(exp_tensor, sum_tensor);
 
     return result;
 }
